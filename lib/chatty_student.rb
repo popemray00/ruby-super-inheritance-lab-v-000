34189bc607607b5020b00a3f72acf7e1c4c21ebb
Michael Pope <popemray1986@gmail.com>
2017-12-10 01:12:50 +0000

    automatically backed up by learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -7,16 +7,8 @@ class ChattyStudent < Student
 
   def raise_hand
     super
-     puts "Pick me!"
-     puts "Pick me!"
-     puts "Pick me!"
-     puts "Pick me!"
-     puts "Pick me!"
-     puts "Pick me!"
-     puts "Pick me!"
-     puts "Pick me!"
-     puts "Pick me!"
-     puts "Pick me!"
+     puts "Pick me!" * 10
+     
 
 
 
